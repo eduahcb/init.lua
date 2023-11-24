da324adb67a63b5b3ef6cccf53fd7d95cdb24924
eduahcb <eduardo.barbosa@rdstation.com>
2023-11-24 11:01:02 -0300

    feat: add trouble

--- lua/eduahcb/packer.lua
+++ lua/eduahcb/packer.lua
@@ -77,4 +77,16 @@ use {
     'olimorris/neotest-rspec'
   },
 }
+
+use {
+	"folke/trouble.nvim",
+	requires = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("trouble").setup {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		}
+	end
+}
 end)
