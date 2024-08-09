a77e20326d191ca36ab1b39308c8f46204411d60
eduahcb <dev_eduardo@hotmail.com>
2024-08-09 08:16:08 -0300

    update

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,18 @@
 local builtin = require('telescope.builtin')
+local actions = require("telescope.actions")
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+require('telescope').setup{
+  defaults= {
+    mappings = {
+      n = {
+        ["<leader>j"] = actions.close
+      }
+    }
+  }
+}
--- lua/eduahcb/remap.lua
+++ lua/eduahcb/remap.lua
@@ -1,4 +1,8 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>n", vim.cmd.Ex)
+vim.keymap.set("n", "<leader><leader>", ":w<CR>")
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("n", "<leader>j", "<Esc>")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
